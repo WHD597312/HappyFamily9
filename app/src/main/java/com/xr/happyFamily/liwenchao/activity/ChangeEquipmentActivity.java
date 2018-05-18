10a836baf3f03cb16bca0f1816db307227a0c206
jiangjieyu <jiananqi@foxmail.com>
2018-05-18 10:17:17 +0800

    登录和家

---
+++ app/src/main/java/com/xr/happyFamily/liwenchao/activity/ChangeEquipmentActivity.java
@@ -0,0 +1,60 @@
+package com.xr.happyFamily.liwenchao.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+import com.xr.happyFamily.R;
+import com.xr.happyFamily.liwenchao.adapter.MyAdapter;
+import com.xr.happyFamily.liwenchao.pojo.Equipment;
+import com.xr.happyFamily.liwenchao.pojo.xnsb;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
+
+public class ChangeEquipmentActivity extends AppCompatActivity {
+
+    private List<String> mData = new ArrayList<String>(Arrays.asList("智能终端升级版", "户外空调", "智能插座基础版", "空气净化器"));
+    private Context context;
+    Unbinder unbinder ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getSupportActionBar() != null){
+            getSupportActionBar().hide();
+        }
+        context = this;
+        setContentView(R.layout.activity_home_xnsb);
+        unbinder = ButterKnife.bind(this);
+        ListView listView = (ListView) findViewById(R.id.xnsb_list);
+        MyAdapter mAdapter = new MyAdapter(context, mData);
+        listView.setAdapter(mAdapter);
+    }
+    @OnClick({R.id.ib_xnty})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.ib_xnty:
+              finish();
+                break;
+
+        }
+
+    }
+
+
+}
+
+
+
+
+
---
+++ app/src/main/java/com/xr/happyFamily/liwenchao/adapter/RoomAdapter.java
@@ -0,0 +1,34 @@
+package com.xr.happyFamily.liwenchao.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.xr.happyFamily.R;
+import com.xr.happyFamily.liwenchao.pojo.Room;
+import java.util.List;
+
+public class RoomAdapter extends ArrayAdapter {
+    private final int resourceId;
+
+    public RoomAdapter(Context context, int textViewResourceId, List<Room> objects) {
+       super(context, textViewResourceId, objects);
+        resourceId = textViewResourceId;
+
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Room room = (Room) getItem(position); // 获取当前项的Room实例
+        View view = LayoutInflater.from(getContext()).inflate(resourceId, null);//实例化一个对象
+        ImageView roomImage = (ImageView) view.findViewById(R.id.iv_change);//获取该布局内的图片视图
+        TextView roomName = (TextView) view.findViewById(R.id.tv_change_1);//获取该布局内的文本视图
+        roomImage.setImageResource(room.getImgId());//为图片视图设置图片资源
+        roomName.setText(room.getName());//为文本视图设置文本内容
+        return view;
+
+    }
+
+}

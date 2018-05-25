e83d4391738bf1f6daefb1c7d39ea784eff8b182
WHD597312 <1352250152@qq.com>
2018-05-25 21:10:56 +0800

    搭建添加设备页面，二维码扫描

--- app/src/main/java/com/xr/happyFamily/jia/activity/AddDeviceActivity.java
+++ app/src/main/java/com/xr/happyFamily/jia/activity/AddDeviceActivity.java
@@ -26,7 +26,7 @@ public class AddDeviceActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_device);
         unbinder=ButterKnife.bind(this);
-        
+
     }
 
     private int position=-1;

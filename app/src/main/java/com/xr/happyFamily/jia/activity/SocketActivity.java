0ddd46f1c508e78e1c2a1bc8c00f5bbfb7d27e4a
WHD597312 <1352250152@qq.com>
2018-07-26 10:44:26 +0800

    家（取暖器设备详情）

---
+++ app/src/main/java/com/xr/happyFamily/jia/activity/SocketActivity.java
@@ -0,0 +1,15 @@
+package com.xr.happyFamily.jia.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xr.happyFamily.R;
+
+public class SocketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_socket);
+    }
+}

a5af2aa94e208c7c0d861d34993ce0e7825d200a
WHD597312 <1352250152@qq.com>
2018-09-11 20:05:17 +0800

    修改取暖器设备（页面）

--- app/src/main/java/com/xr/happyFamily/zhen/PersonInfoActivity.java
+++ app/src/main/java/com/xr/happyFamily/zhen/PersonInfoActivity.java
@@ -10,6 +10,7 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -147,6 +148,7 @@ public class PersonInfoActivity extends AppCompatActivity {
                                 long maxTime=date3.getTime();
                                 TimePickerDialog dialogYearMonthDay = new TimePickerDialog.Builder()
                                         .setType(Type.YEAR_MONTH_DAY)
+                                        .setThemeColor(Color.parseColor("#4fc586"))
                                         .setTitleStringId("修改生日")
                                         .setMinMillseconds(minDay)
                                         .setCurrentMillseconds(current)

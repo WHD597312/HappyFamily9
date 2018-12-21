0cb77437a8077002184819349b4adbdb71d2a878
WHD597312 <1352250152@qq.com>
2018-12-21 09:35:18 +0800

    ssssssssssssss

---
+++ app/src/main/java/com/xr/happyFamily/together/util/mqtt/UtilsService.java
@@ -0,0 +1,29 @@
+package com.xr.happyFamily.together.util.mqtt;
+
+import android.app.ActivityManager;
+import android.content.Context;
+
+import java.util.ArrayList;
+
+public class UtilsService {
+    /**
+     * 判断服务是否开启
+     *
+     * @return
+     */
+    public static boolean isServiceRunning(Context context, String ServiceName) {
+        if (("").equals(ServiceName) || ServiceName == null)
+            return false;
+        ActivityManager myManager = (ActivityManager) context
+                .getSystemService(Context.ACTIVITY_SERVICE);
+        ArrayList<ActivityManager.RunningServiceInfo> runningService = (ArrayList<ActivityManager.RunningServiceInfo>) myManager
+                .getRunningServices(30);
+        for (int i = 0; i < runningService.size(); i++) {
+            if (runningService.get(i).service.getClassName().toString()
+                    .equals(ServiceName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
